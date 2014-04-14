0b623506287e8ae5eb13aa6c3ff9669019441c0b
António Soares <asoaresmail@gmail.com>
2014-04-14 22:15:14 +0100

    forgot to change version --'

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 
 @section('content')
-	<p> Teste de Git pull automático por parte do servidor v1.4</p>
+	<p> Teste de Git pull automático por parte do servidor v1.5</p>
 	@if(Auth::check())
 		<p>Hello, {{ Auth::user()->username }}.</p>
 	@else
