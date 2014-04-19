e10d6b4e38ec794f54af481a92f0d1a1766f1b9e
tjiagoM <tiago.manuel@gmail.com>
2014-04-19 12:08:00 +0100

    HomeController edited for merge

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,37 +1,14 @@
 <?php
 
-class HomeController extends BaseController {
-
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
-
+class HomeController extends BaseController 
+{
 	public function showWelcome()
 	{
-		/*Publication::create(array(
-			'initial_date'         => date('Y-m-d H:i:s'),
-			'final_date'         => date('Y-m-d H:i:s'),
-			'is_public'				 => true,
-			'periodic_notification' => 1,
-			'risk' => 3,
-			'type' => 'alert'
-		));*/
-$contents = Publication::find(4)->contents();
-var_dump($contents);
+		//$request = Request::create('/publications', 'GET', array());
+		//$publications = Route::dispatch($request)->getContent();
 	
-	foreach ($contents as $content)
-		echo $content->title . ' ' . $content->content;
-
-		//return View::make('pages.home');
+		$publications = PublicationController::getPublications();
+		
+		return View::make('pages.home')->with('publications', $publications);
 	}
-
 }
