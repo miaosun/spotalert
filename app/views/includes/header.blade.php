85025e854fea6620b0832e0e14bf835737fbd2d1
unknown <carloslopesamaral@outlook.com>
2014-04-19 18:46:40 +0100

    2.1 css

--- app/views/includes/header.blade.php
+++ app/views/includes/header.blade.php
@@ -6,7 +6,7 @@
 			<li><a href="/taqueto">Filter</a></li>
 			<li><a href="/poetemanso">Contacts</a></li>
             <li><a href="/tirapata">Log in</a></li>
-            <li><form id="search" name="search" action="/search" method="get">
+            <li><form id="search" name="search" action="/search" method="get"></form>
             <input id="search-input" placeholder=" 🔍 SEARCH"name="Search" type="text"></li>
             <li><a href="/en">EN</a></li><li><a href="/pt">PT</a></li>
 		</ul>
