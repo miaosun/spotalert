fc621b628bdc7aedd3e5416c3afed85f233c66f1
Miao Sun <miaosun88@gmail.com>
2014-05-10 00:08:28 +0100

    must accept terms in registration

--- app/views/account/create.blade.php
+++ app/views/account/create.blade.php
@@ -3,28 +3,31 @@
 @section('content')
 <div class="container-fluid">
     <div id="create" class="col-md-8 col-md-offset-2">
-        <div class="row">
-            <br>
-            <h1>REGISTER</h1>
-            <div class="col-md-5 col-md-offset-0" id="moveright">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h5>TERMS OF SERVICE</h5>
-                        <textarea rows="13" cols="32" readonly>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</textarea>
+        <form action="{{ URL::route('account-create-post') }}" method="post">
+            <div class="row">
+                <br>
+                <h1>REGISTER</h1>
+                <div class="col-md-5 col-md-offset-0" id="moveright">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h5>TERMS OF SERVICE</h5>
+                            <textarea rows="13" cols="32" readonly>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</textarea>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <p>Do you accept the terms of service?</p>
+                            YES {{ Form::radio('accept', 'yes') }}
+                            NO {{ Form::radio('accept', 'no') }}
+                            @if($errors->has('accept'))
+                            <br><span>YOU MUST ACCEPT THE TERMS</span>
+                            @endif
+                        </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <p>Do you accept the terms of service?</p>
-                        YES {{ Form::radio('accept', 'yes', true) }}
-                        NO {{ Form::radio('accept', 'no') }}
-                    </div>
-                </div>
-            </div>
 
 
-            <div class="col-md-7 col-md-offset-0">
-                <form action = "{{ URL::route('account-create-post') }}" method="post">
+                <div class="col-md-7 col-md-offset-0">
                     <div class="row">
                         <div class="col-md-4">
                             {{ Form::label('username *',null,array('class' => 'label')) }}
@@ -56,7 +59,8 @@
                             {{ Form::label('first name',null,array('class' => 'label')) }}
                         </div>
                         <div class="col-md-7 col-md-offset-0">
-                            {{ Form::text('firstname', Input::old('firstname')  , array('placeholder'=>'ADD FIRST NAME')) }}
+                            {{ Form::text('firstname', Input::old('firstname') , array('placeholder'=>'ADD FIRST NAME'))
+                            }}
                         </div>
                     </div>
                     <div class="row">
@@ -137,11 +141,10 @@
                             {{ Form::submit('REGISTER') }}
                         </div>
                     </div>
-                    {{ Form::token() }}
-                </form>
+                </div>
             </div>
-        </div>
-        {{ Form::close() }}
+            {{ Form::token() }}
+            {{ Form::close() }}
     </div>
 </div>
 @stop
