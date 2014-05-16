28840c1f3717424d8187c0c61a9eeed85b95520c
unknown <carloslopesamaral@outlook.com>
2014-05-16 02:15:40 +0100

    Update 4.2 CSS

--- app/views/publications/filter.blade.php
+++ app/views/publications/filter.blade.php
@@ -1,16 +1,21 @@
 <div class="filter-opt filter-risk">{{Lang::get('home.filter.risk_extreme')}}</div>
+    <hr>
 <div class="filter-opt filter-risk">{{Lang::get('home.filter.risk_medium')}}</div>
+    <hr>
 <div class="filter-opt filter-risk">{{Lang::get('home.filter.risk_low')}}</div>
+    <hr>
 <div>
     <a class="trigger right-caret">{{Lang::get('home.filter.type')}}</a>
     <div class="dropdown-menu sub-menu">
         <div>
         @foreach (EventType::all() as $eventType)
             <div class="filter-opt filter-eventType">{{{$eventType->name}}}</div>
+            <hr>
         @endforeach
         </div>
         <div class="filter-ok">OK</div>
     </div>
+        <hr>
 </div>
 <div>
     <a class="trigger right-caret">{{Lang::get('home.filter.country')}}</a>
@@ -18,11 +23,12 @@
         <div class="filter-opt filter-all">All countries</div>
         <div style="height:80px;width:218px;" class="scrollable" style="overflow-y: scroll;">
         
-        @foreach (Country::all() as $country)
-        <div class="filter-opt filter-country">{{{$country->name}}}</div>
-        @endforeach
+            @foreach (Country::all() as $country)
+            <div class="filter-opt filter-country">{{{$country->name}}}</div>
+            @endforeach
         
         </div>
+        <hr>
         <div class="filter-ok">OK</div>
     </div>
 </div>
