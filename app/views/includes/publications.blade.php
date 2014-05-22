912e0590865fa7e15f24abef58b87f3049a93a1c
António Soares <asoaresmail@gmail.com>
2014-05-22 20:02:37 +0100

    add support to language add sprite for social shares finish show comments (check that language thing and active thing) TODO: no images yet because no support in db. only load active comments

--- app/views/includes/publications.blade.php
+++ app/views/includes/publications.blade.php
@@ -3,112 +3,94 @@
 @if(isset($next_page) && $next_page == 2)
 <div class="scroll">
 @endif
+    
+    @foreach ($publications as $key => $publication)
 
-	@foreach ($publications as $key => $publication)
+    @if (($key % 3) == 0)
+    <div class="row row-publ">
+    @endif
+    <div class="col-md-4">
+        <div class="col-md-12 col-sm-12 publication-{{{$publication['type']}}} publ-risk{{{$publication['risk']}}}"
+            id="publ-{{ $publication['id'] }}">
 
-	@if (($key % 3) == 0)
-	<div class="row row-publ">
-	@endif
-		<div class="col-md-4">
-			<div class="{{(isset($publication['inactive'])) ? 'inactive' : ''}} col-md-12 col-sm-12 publication-{{{$publication['type']}}} publ-risk{{{$publication['risk']}}}"
-				id="publ-{{ $publication['id'] }}">
-				
-				<div class="publ_header">
-					
-					<div class="col-md-4 publ-risk">
-					@if ($publication['type'] == 'alert')
+            <div class="publ_header">
+                
+                <div class="col-md-4 publ-risk">
+                @if ($publication['type'] == 'alert')
 
 
-						@if($publication['risk'] >= 1 && $publication['risk'] <=2)
-							<div class="ray_low"></div>
-						@endif
-
-						@if($publication['risk'] >= 3 && $publication['risk'] <=4)
-							<div class="ray_medium"></div>
-						@endif
-
-						@if($publication['risk'] >= 5)
-							<div class="ray_high"></div>
-						@endif
-
-							@else
-								<div class="plus"></div>
-
-							@endif
-					</div>
-					<div class="col-md-4 publ-type">
-						@foreach ($publication['event_types'] as $eventType)
-							{{{$eventType}}}
-						@endforeach
-					</div>
-					
-					<div class="col-md-4 publ-countr">
-						@foreach ($publication['affected_countries'] as $country)
-							{{{$country}}}<br>
-						@endforeach
-					</div>
-				</div>
-		<br>
-		<hr>
-		<div class="publ_body">
-			<div class="publ-title">{{{$publication['title']}}}</div>
-            <div class="publ-colapse">
-                <hr>
-                <div class="publ-content">
-                    <p></p>
-                    <div class="publ-linked">
-                    @if ($publication['type'] == 'alert')
-                        <h1>GUIDELINES 
-                        @if($publication['risk'] >=5)
-                            <button class="glyphicon glyphicon-chevron-right arrow_white publ-linked-toggle-btn" publicationid="{{ $publication['id'] }}"></button>
-                        @else
-                            <button class="glyphicon glyphicon-chevron-right arrow_gray publ-linked-toggle-btn" publicationid="{{ $publication['id'] }}"></button>
-                        @endif        
-                        </h1>        
-                    @else
-                        <h1>ALERTS 
-                        @if($publication['risk'] >=5)
-                            <button class="glyphicon glyphicon-chevron-right arrow_white publ-linked-toggle-btn" publicationid="{{ $publication['id'] }}"></button>
-                        @else
-                            <button class="glyphicon glyphicon-chevron-right arrow_gray publ-linked-toggle-btn" publicationid="{{ $publication['id'] }}"></button>
-                        @endif
-                        </h1>
+                    @if($publication['risk'] >= 1 && $publication['risk'] <=2)
+                        <div class="ray_low"></div>
                     @endif
-                    <div class="publ-linked-toggle"></div>
-                    </div>
-                    <h1>SHARE</h1>
-                    <!-- facebook -->
-                    <a href="https://www.facebook.com/sharer/sharer.php?u={http://spotalert.fe.up.pt/publication/{{{$publication['id']}}}"
-               onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');shareFacebook({{{$publication['id']}}});return false;"
-               target="_blank" title="Share on Facebook">
-                        <img src="https://www.gstatic.com/images/icons/gplus-16.png" alt="Share on Facebook"/></a>
-                    </a>
-                    <!-- google+ -->
-                    <a href="https://plus.google.com/share?url={http://spotalert.fe.up.pt/publication/{{{$publication['id']}}}" onclick="javascript:window.open(this.href,
-              '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');shareGoogle({{{$publication['id']}}});return false;" target="_blank" title="Share on Google+">
-                        <img src="https://www.gstatic.com/images/icons/gplus-16.png" alt="Share on Google+"/>
-                    </a>
-                    <!-- twitter -->
-                    <a href="https://twitter.com/share?url=http://spotalert.fe.up.pt/publication/{{{$publication['id']}}}&hashtags=SPOTALERT&text={{{$publication['title']}}}"
-               onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');shareTwitter({{{$publication['id']}}});return false;"
-               target="_blank" title="Share on Twitter">
-                        <img src="https://www.gstatic.com/images/icons/gplus-16.png" alt="Share on Twitter"/>
-                    </a>
-                    <!-- linkedIn -->
-                    <a href="http://www.linkedin.com/shareArticle?mini=true&url=http://spotalert.fe.up.pt/publication/{{{$publication['id']}}}&title={{{$publication['title']}}}" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');shareLinkdIn({{{$publication['id']}}});return false;"
-               target="_blank" title="Share on LinkdIn">
-                        <img src="https://www.gstatic.com/images/icons/gplus-16.png" alt="Share on LinkdIn"/>
-                    </a>
-                    @if ($publication['risk'] >=5 && $publication['type'] == 'alert')
-                        <h1>COMMENTS <button class="glyphicon glyphicon-chevron-right arrow_white publ-comment-toggle-btn" publicationid="{{ $publication['id'] }}"></button></h1>
-                    @else
-                        <h1>COMMENTS <button class="glyphicon glyphicon-chevron-right arrow_gray publ-comment-toggle-btn" publicationid="{{ $publication['id'] }}"></button></h1>
+
+                    @if($publication['risk'] >= 3 && $publication['risk'] <=4)
+                        <div class="ray_medium"></div>
                     @endif
-                    <div class="publ-comments-colapse"></div>
+
+                    @if($publication['risk'] >= 5)
+                        <div class="ray_high"></div>
+                    @endif
+
+                    @else
+                        <div class="plus"></div>
+
+                    @endif
+                </div>
+                <div class="col-md-4 publ-type">
+                    @foreach ($publication['event_types'] as $eventType)
+                        {{{$eventType}}}
+                    @endforeach
+                </div>
+
+                <div class="col-md-4 publ-countr">
+                    @foreach ($publication['affected_countries'] as $country)
+                        {{{$country}}}<br>
+                    @endforeach
                 </div>
             </div>
+
+            <br>
             <hr>
-				@if(isset($publication['hidden']))
+            <div class="publ_body">
+                <div class="publ-title">{{{$publication['title']}}}</div>
+                <div class="publ-colapse">
+                    <hr>
+                    <div class="publ-content">
+                        <p></p>
+                        <div class="publ-linked">
+                        @if ($publication['type'] == 'alert')
+                            <h1>{{ Lang::get('publication.titles.guide') }} 
+                            @if($publication['risk'] >=5)
+                                <button class="glyphicon glyphicon-chevron-right arrow_white publ-linked-toggle-btn" publicationid="{{ $publication['id'] }}"></button>
+                            @else
+                                <button class="glyphicon glyphicon-chevron-right arrow_gray publ-linked-toggle-btn" publicationid="{{ $publication['id'] }}"></button>
+                            @endif        
+                            </h1>        
+                        @else
+                            <h1>{{ Lang::get('publication.titles.alert') }} 
+                            @if($publication['risk'] >=5)
+                                <button class="glyphicon glyphicon-chevron-right arrow_white publ-linked-toggle-btn" publicationid="{{ $publication['id'] }}"></button>
+                            @else
+                                <button class="glyphicon glyphicon-chevron-right arrow_gray publ-linked-toggle-btn" publicationid="{{ $publication['id'] }}"></button>
+                            @endif
+                            </h1>
+                        @endif
+                        <div class="publ-linked-toggle"></div>
+                        </div>
+                        @include('publications.publicationShare')
+
+                        <div class="publ-comments">
+                            @if ($publication['risk'] >=5 && $publication['type'] == 'alert')
+                                <h1>{{ Lang::get('publication.titles.comments.title') }} <button class="glyphicon glyphicon-chevron-right arrow_white publ-comments-toggle-btn" publicationid="{{ $publication['id'] }}"></button></h1>
+                            @else
+                                <h1>{{ Lang::get('publication.titles.comments.title') }}  <button class="glyphicon glyphicon-chevron-right arrow_gray publ-comments-toggle-btn" publicationid="{{ $publication['id'] }}"></button></h1>
+                            @endif
+                            <div class="publ-comments-toggle"></div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                @if(isset($publication['hidden']))
 					<div class="publ-info publ-hidden">{{Lang::get('home.publications.hidden')}}</div>
 				@elseif(isset($publication['inactive']))
 					<div class="publ-info publ-inactive">{{Lang::get('home.publications.inactive')}}</div>
@@ -117,7 +99,7 @@
 				@elseif(isset($publication['updated']))
 					<div class="publ-info upd-red">{{Lang::get('home.publications.updated')}}</div>
 				@endif
-        </div>
+            </div>
 		<div class="col-md-12 publ_footer">
 			@if(Auth::check() && Auth::user()->type != 'normal')
 			<div class="row">
@@ -146,7 +128,6 @@
 	@if (($key % 3) == 2 || $key == count($publications) - 1)
 	</div>
 	@endif
-
 @endforeach
 
 @if(isset($next_page))
@@ -181,9 +162,7 @@
         <button type="button" class="btn btn-default" data-dismiss="modal">{{Lang::get('home.del-publ.no')}}</button>
       </div>
     </div>
-  </div>
-</div>
-@endif
+    @endif
 @endif
 
 @endif
---
+++ app/views/publications/publicationShare.blade.php
@@ -0,0 +1,25 @@
+<h1>{{ Lang::get('publication.titles.social') }}</h1>
+<div>
+    <!-- facebook -->
+    <a href="https://www.facebook.com/sharer/sharer.php?u={http://spotalert.fe.up.pt/publication/{{{$publication['id']}}}"
+    onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');shareFacebook({{{$publication['id']}}});return false;"
+    target="_blank" title="Share on Facebook">
+        <span class="sprite-social social-facebook" alt="Share on Facebook"></span>
+    </a>
+    <!-- google+ -->
+    <a href="https://plus.google.com/share?url={http://spotalert.fe.up.pt/publication/{{{$publication['id']}}}" onclick="javascript:window.open(this.href,
+    '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');shareGoogle({{{$publication['id']}}});return false;" target="_blank" title="Share on Google+">
+        <span class="sprite-social social-google" alt="Share on Google+"></span>
+    </a>
+    <!-- twitter -->
+    <a href="https://twitter.com/share?url=http://spotalert.fe.up.pt/publication/{{{$publication['id']}}}&hashtags=SPOTALERT&text={{{$publication['title']}}}"
+    onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');shareTwitter({{{$publication['id']}}});return false;"
+    target="_blank" title="Share on Twitter">
+        <span class="sprite-social social-twitter" alt="Share on Twitter"></span>
+    </a>
+    <!-- linkedIn -->
+    <a href="http://www.linkedin.com/shareArticle?mini=true&url=http://spotalert.fe.up.pt/publication/{{{$publication['id']}}}&title={{{$publication['title']}}}" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');shareLinkdIn({{{$publication['id']}}});return false;"
+    target="_blank" title="Share on LinkdIn">
+        <span class="sprite-social social-linkdin" alt="Share on LinkdIn"></span>
+    </a>
+</div>
