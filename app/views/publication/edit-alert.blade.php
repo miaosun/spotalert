a0d9d6d6050c13be55aa384591c982093be8b550
Guilherme Martins <luisgui2007@hotmail.com>
2014-05-30 02:14:08 +0100

    Edit publication rebuilt

--- app/views/publication/edit-alert.blade.php
+++ app/views/publication/edit-alert.blade.php
@@ -8,150 +8,164 @@
     $idp = $publication['id'];
 ?>
 
-<div class="content">
-    {{ Form::open(['url' => '/publication/editalert']) }}
-    <div id="alert-tabs">
-        <div id="languages_tabs">
-            <ul class="nav nav-tabs">
-                <li class="active">
-                    <a id="alert_english_tab" href="#alert_english" data-toggle="tab">English</a>
-                </li>
-                @foreach ($contents as $content)
+
+<div class="container-fluid">
+    <div id="create-alert" class="col-md-8 col-sm-8 col-md-offset-2 general_panel">
+        {{ Form::open(['url'=>'/publication/editalert']) }}
+        <div id="alert-tabs">
+            <div id="languages_tabs">
+                <ul class="nav nav-tabs">
+                    <li class="active">
+                        <a id="alert_english_tab" href="#alert_english" data-toggle="tab">English</a>
+                    </li>
+                    @foreach ($contents as $content)
                     <li>
-                        <a id="alert_tab_{{$content['language_id']}}" href="#alert_{{$content['language_id']}}" data-toggle='tab'>{{ $language_options[$content['language_id']]}}</a>
-                        <span id='rm-language' class='glyphicon glyphicon-remove'></span>
+                        <a id="alert_tab_{{$content['language_id']}}" href="#alert_{{$content['language_id']}}" data-toggle='tab'>{{$language_options[$content['language_id']]}}<span id='rm-language' class='glyphicon glyphicon-remove' style='padding-left: 5px'></span></a>
                     </li>
                     <?php
-                        $lang_index =  array_search($language_options[$content['language_id']], $language_options);
+                        $lang_index = array_search($language_options[$content['language_id']], $language_options);
                         unset($language_options[$lang_index]);
                     ?>
-                @endforeach
-                <li>
-                    <a id="addlanguage_tab" href="#addlanguage" data-toggle="tab">+ Add Language</a>
-                </li>
-            </ul>
-            <div class="modal fade add-language-modal" tabindex="-1" role="dialog" aria-labelledby="AddLanguage" aria-hidden="true">
-                <div class="modal-dialog modal-sm">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                            <h4 class="modal-title">Add Language</h4>
+                    @endforeach
+                    <li>
+                        <a id="addlanguage_tab" href="#addlanguage" data-toggle="tab">+ {{Lang::get('create-alert.addtab')}}</a>
+                    </li>
+                </ul>
+                <div class="modal fade add-language-modal" tabindex="-1" role="dialog" aria-labelledby="AddLanguage" aria-hidden="true">
+                    <div class="modal-dialog modal-sm">
+                        <div class="modal-content"> 
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                <h4 class="modal-title">{{Lang::get('create-alert.addtab')}}</h4>
+                            </div>
+                            <div class="modal-body">
+                                <select id="languages-select"></select>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">{{Lang::get('create-alert.modals.cancel')}}</button>
+                                <button type="button" class="btn btn-primary" id="choose_language_button">{{Lang::get('create-alert.modals.confirm')}}</button>
+                            </div>
                         </div>
-                        <div class="modal-body">
-                            <select id="languages-select"></select>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                            <button type="button" class="btn btn-primary" id="choose_language_button">Confirm</button>
+                    </div>
+                </div>
+                <div class="modal fade rm-language-modal" tabindex="-1" role="dialog" aria-labelledby="RemoveLanguage" aria-hidden="true">
+                    <div class="modal-dialog modal-sm">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                <h4 class="modal-title">{{Lang::get('create-alert.modals.removelang')}}</h4>
+                            </div>
+                            <div class="modal-body">
+                                <p>{{Lang::get('create-alert.modals.removemsg')}}</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">{{Lang::get('create-alert.modals.cancel')}}</button>
+                                <button type="button" class="btn btn-primary" id="rm_language_button">{{Lang::get('create-alert.modals.confirm')}}</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="modal fade rm-language-modal" tabindex="-1" role="dialog" aria-labelledby="RemoveLanguage" aria-hidden="true">
-                <div class="modal-dialog modal-sm">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                            <h4 class="modal-title">Remove Language</h4>
+
+            <div class="row tab-content" >
+                <div class="tab-pane fade in active" id="alert_english">
+                    <h1>{{Lang::get('create-alert.edit_alert')}}</h1>
+                    <div class="col-md-5 col-sm-5 col-md-offset-0" id="moveright">
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12">
+                                <h5>{{Lang::get('create-alert.fields.title')}} <span>{{Lang::get('create-alert.fields.max_size')}}</span></h5>
+                                {{ Form::textarea('alert-title', $all_contents[0]['title'], array('id'=>'title', 'placeholder'=>Lang::get('create-alert.placeholders.title'))) }}
+                            </div>
                         </div>
-                        <div class="modal-body">
-                            <p>Are you sure you want to remove this language? All the information will be lost.</p>
+
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12">
+                                <h5>{{Lang::get('create-alert.fields.description')}}</h5>
+                                {{ Form::textarea('alert-description', $all_contents[0]['content'], array('placeholder'=>Lang::get('create-alert.placeholders.description'))) }}
+                            </div>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                            <button type="button" class="btn btn-primary" id="rm_language_button">Confirm</button>
+
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12">
+                                <h5>{{Lang::get('create-alert.fields.images')}}</h5>
+                                {{ Form::file('alert-images[]',array('multiple')) }}
+                                @if($errors->has('file'))
+                                <br><span>{{ $errors->first('file') }}</span>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col-md-6 col-sm-6 col-sm-offset-1 col-md-offset-1">
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12">
+                                <h5>{{Lang::get('create-alert.fields.affected_countries')}}</h5>
+                                {{ Form::select('alert-countries[]', $country_options, $countries,  array('class' => 'chosen-select', 'multiple', 'data-placeholder' => Lang::get('create-alert.placeholders.countries')))}}
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12">
+                                <h5>{{Lang::get('create-alert.fields.duration')}}</h5>
+                                <div class="inrow">
+                                    <span>{{Lang::get('create-alert.labels.from')}}</span>
+                                    {{Form::input('text', 'alert-durationfrom', $publication->initial_date, array('class'=>'datepicker'))}} 
+                                    <span>{{Lang::get('create-alert.labels.from')}}</span>
+                                    {{Form::input('text', 'alert-durationto', $publication->final_date, array('class'=>'datepicker'))}}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12 inrow">
+                                <h5 style="float: left">{{Lang::get('create-alert.fields.eventrisk')}}</h5>
+                                <span>{{ Form::selectRange('alert-risk', 1, 5, $publication->risk);}}</span>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12">
+                                <h5>{{Lang::get('create-alert.fields.eventtype')}}</h5>
+                                {{ Form::select('alert-types[]', $event_type_options, $types,  array('class' => 'chosen-select', 'multiple', 'data-placeholder' => Lang::get('create-alert.placeholders.types')))}}
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12">
+                                <h5>{{Lang::get('create-alert.fields.visibility')}}</h5>
+                                <div class="inrow">
+                                    <span>{{Lang::get('create-alert.labels.public')}}</span>
+                                    {{ Form::radio('alert-visibility', 1, false)}}
+                                    <span>{{Lang::get('create-alert.labels.hidden')}}</span>
+                                    {{Form::radio('alert-visibility', 0, true);}}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12">
+                                <h5>{{Lang::get('create-alert.fields.guidelines')}}</h5>
+                                {{ Form::select('alert-guidelines[]', $guideline_options, $guidelines,  array('class' => 'chosen-select', 'multiple', 'data-placeholder' => Lang::get('create-alert.placeholders.guidelines')))}}
+                            </div>
+                        </div>
+                        <input type="hidden" name="alert-languages" display="none">
+                        <div class="row last-row">
+                            <div class="col-md-4" id="mand_field">
+                                <br>{{ Lang::get('create-alert.mandatory') }}
+                            </div>
+                            <div class="col-md-6 pull-right">
+                                <button id="activate_publication" class="actbuttons">{{Lang::get('create-alert.activate')}}</button>
+                                <button id="deactivate_publication" class="actbuttons">{{Lang::get('create-alert.deactivate')}}</button>
+                                {{  Form::submit(Lang::get('create-alert.submit'), array('class' => 'alert-submit'));
+                                Form::close()}}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-
-        <div class="tab-content" >
-            <div class="tab-pane fade in active" id="alert_english">
-                <h2 id="alert-header">Edit alert</h2>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        {{ Form::label('alert-title-label', 'Title');
-                            echo Form::text('alert-title', $all_contents[0]['title']);}}
-                    </div>
-
-                    <div class="form-group">
-                        {{ Form::label('alert-description-label', 'Description');
-                        echo Form::textarea('alert-description', $all_contents[0]['content'])}}
-                    </div>
-
-                    <div class="form-group">
-                        {/*Form::file($name, $attributes = array());*/}
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        {{Form::label('alert-countries-label', 'Countries Affected');
-                        echo Form::select('alert-countries[]', $country_options, $countries,  array('class' => 'chosen-select', 'multiple', 'data-placeholder' => 'Pick at least one country'))}} 
-                    </div>
-
-                    <div class="form-group">
-                        {{Form::label('alert-duration-label', 'Duration')}}
-                        <div class="inrow">
-                            {{ Form::label('alert-durationfrom-label', 'From');
-                            echo Form::input('text', 'alert-durationfrom', $publication->initial_date, array('class'=>'datepicker'));
-                            echo Form::label('alert-durationto-label', 'To');
-                            echo Form::input('text', 'alert-durationto', $publication->final_date, array('class'=>'datepicker'));}}
-                        </div>
-                    </div>
-
-                    <div class="form-group inrow">
-                        {{ Form::label('alert-risk-label', 'Event Risk', ['id'=>'alert-risk-label']);
-                        echo Form::selectRange('alert-risk', 1, 5, $publication->risk);}}
-                    </div>
-
-                    <div class="form-group">
-                        {{ Form::label('alert-types-label', 'Event Type');
-                        echo Form::select('alert-types[]', $event_type_options, $types,  array('class' => 'chosen-select', 'multiple', 'data-placeholder' => 'Pick at least one type of event'))}}
-                    </div>
-
-                    <div class="form-group">
-                        {{ Form::label('alert-visibility-label', 'Visibility');}}
-                        <div class="inrow">
-                            {{Form::label('alert-radiopublic-label', 'Public');
-                            echo Form::radio('alert-visibility', 1, false);
-                            echo Form::label('alert-radiohidden-label', 'Hidden');
-                            echo Form::radio('alert-visibility', 0, true);}}
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        {{ Form::label('alert-guidelines-label', 'Guidelines');
-                        echo Form::select('alert-guidelines[]', $guideline_options, $guidelines,  array('class' => 'chosen-select', 'multiple', 'data-placeholder' => 'Pick at least one guideline'))}}
-                    </div>
-                </div>
-                @if($errors->has())
-                @foreach($errors->all() as $error)
-                <div data-alert class="alert-box warning round">
-                    {{$error}}
-                    <a href="#" class="close">&times;</a>
-                </div>
-                @endforeach
-                @endif
-            </div>
-            @foreach($contents as $content)
-                <div class="tab-pane fade in" id="alert_{{$content['language_id']}}">
-                    <div class="form-group">
-                        <label for="alert-title-label">Title</label>
-                        <input name="alert-title{{$content['language_id']}}" type="text" value="{{$content['title']}}">
-                    </div>
-                    <div class="form-group">
-                        <label for="alert-description-label">Description</label>
-                        <textarea name="alert-description{{$content['language_id']}}" cols="50" rows="10">{{$content['content']}}</textarea>
-                    </div>
-                </div>
-            @endforeach
         </div>
     </div>
-    <input type="hidden" name="alert-languages" display="none">
-    <input type="hidden" name="alert-id" display="none" value="{{$idp}}">
-    {{  Form::submit('Submit', array('class' => 'alert-submit'));
-    Form::close()}}
 </div>
 {{ HTML::style('assets/css/chosen.css'); }}
 {{ HTML::script('assets/js/createalert.js') }}
@@ -165,7 +179,7 @@
     for (var selector in config) {
         $(selector).chosen(config[selector]);
     }
-    
+
     $('.datepicker').datepicker({
         format: "yyyy-mm-dd",
         autoclose: true,
@@ -173,6 +187,7 @@
     });
 
     //languages
+
     <?php
         $js_languages = json_encode($language_options);
         $js_contents = [];
@@ -182,19 +197,18 @@
         $js_contents = json_encode($js_contents);
         echo "var language_options = ". $js_languages . ";\n";
         echo "var contents = " . $js_contents . ";\n";
-    
+
     ?>
-    $.each(language_options, function(index, value){
+        $.each(language_options, function(index, value){
 
-        if(value == "English" || value == "undefined")
-        {
-            delete language_options[index];
-        }     
+            if(value == "English")
+            {
+                delete language_options[index];
+            }     
 
-    });
+        });
     function updatelangselect(){
-        
-        $('#languages-select').empty();
+
         var sel = document.getElementById('languages-select');
         $.each(language_options, function(i, val) {
             var opt = document.createElement('option');
@@ -209,6 +223,7 @@
     }
 
     updatelangselect();
+
 </script>
 
 @stop
